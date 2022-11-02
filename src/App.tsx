f07360351e9f6cc567743fda55cc05b2a227a861
Michal Korzeniewski <michal.korzeniewski@allegro.pl>
2022-11-02 12:40:22 +0100

    Add some simple components; GH, LN links and mail button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from "./Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <main className="App--main">
+                <div className= "App--my-photo">
+                    <img src="/images/my-photo.png"/>
+                </div>
+                <div>
+                    <h1 className="full-name">Michał Korzeniewski</h1>
+                    <h4 className="subtitle">Software Engineer</h4>
+                </div>
+                <a href="mailto:me@mkorzeniewski.com"><button className="mail-button">
+                    <img src="/images/Mail.svg"/>
+                    <p>Email</p>
+                </button></a>
+                <div>
+
+                </div>
+                <Footer />
+            </main>
+        </div>
+    );
 }
 
 export default App;
